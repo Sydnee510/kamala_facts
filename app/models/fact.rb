48ffbc11da86b62c7d9c51e8aa123a27f334c7b1
sydnee510 <sbsydnee161416@gmail.com>
2021-07-22 23:08:38 -0700

    added unique validations to Fact model

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -1,7 +1,8 @@
 class Fact < ApplicationRecord
     belongs_to :myth
     accepts_nested_attributes_for :myth, reject_if: :all_blank
-    validates :true_content, :receipt_url, presence: true
+    validates :true_content, presence: true, uniqueness:true
+    validates :receipt_url, presence: true
 
     scope :searched, ->(search_term) { where("true_content LIKE ?", "%#{search_term}%") }
 end
